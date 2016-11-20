cb2a490045dfbf6ca64ba07d31308d5fbd241e1c
MECHERI Akram <mecheri.akram@gmail.com>
2016-11-20 15:46:35 +0100

    Update DbConnection.java

--- sources/src/memoryDatabaseExchange/DbConnection.java
+++ sources/src/memoryDatabaseExchange/DbConnection.java
@@ -14,7 +14,7 @@ public class DbConnection {
 	public static synchronized Statement getStatement() throws SQLException{
 		
 		if (singleConnection == null){
-			singleConnection = DriverManager.getConnection("jdbc:mysql://sql4.freesqldatabase.com:3306/sql4100703","sql4100703","1v4UHg63IL");
+			singleConnection = DriverManager.getConnection("jdbc:mysql://sql4.freesqldatabase.com:3306/sql4100703","sql4100703","pass");
 			if (singleStatement == null){
 				singleStatement = singleConnection.createStatement();
 				System.out.println("Connection Success !");
